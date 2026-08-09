cbd94520556cd10317e106386287488c8a5555c0
ddisanto-langops <1daniel.disanto@gmail.com>
2026-08-08 20:57:56 -0400

    remove testing comments

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,20 @@ router.head("/webhooks/trello", (req, res) => {
 })
 
 router.post("/webhooks/trello", async (req, res) => {
-    //const signature = req.headers['x-trello-webhook'] as string
+    const signature = req.headers['x-trello-webhook'] as string
     const rawWebHook = req.body as TrelloWebhook
     
-    //if (!signature) {
-    //    return res.status(400).send('Missing X-Trello-Webhook header')
-   // }
+    if (!signature) {
+        return res.status(400).send('Missing X-Trello-Webhook header')
+    }
 
     const adapter = new TrelloAdapter()
-    //const isValid = adapter.verifySignature(JSON.stringify(rawWebHook), signature)
+    const isValid = adapter.verifySignature(JSON.stringify(rawWebHook), signature)
 
-    //if (!isValid) {
-      //  console.warn(`Unauthorized webhook attempt blocked from IP: ${req.ip}`)
-       // return res.status(401).send('Unauthorized: invalid signature')
-    //}
+    if (!isValid) {
+        console.warn(`Unauthorized webhook attempt blocked from IP: ${req.ip}`)
+        return res.status(401).send('Unauthorized: invalid signature')
+    }
     
     const timestamp = Date.now()
     console.log(`${timestamp} | IP: ${req.ip} | Status: ${req.statusCode} | Contents: ${JSON.stringify(req.body)}`)
